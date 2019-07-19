7458fdb4972cd68d6db43351347ae6bb823d8ef9
Luigi Pecce <looigi@gmail.com>
2019-07-19 10:40:50 +0200

    Prima versione

--- app/src/main/java/com/looigi/screenoff/Spegne.java
+++ app/src/main/java/com/looigi/screenoff/Spegne.java
@@ -1,5 +1,7 @@
 package com.looigi.screenoff;
 
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +12,10 @@ public class Spegne extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        if (VariabiliGlobali.getInstance().getDevicePolicyManager() == null) {
+            VariabiliGlobali.getInstance().setDevicePolicyManager((DevicePolicyManager) getSystemService(DEVICE_POLICY_SERVICE));
+            VariabiliGlobali.getInstance().setCompName(new ComponentName(this, MyAdmin.class));
+        }
         boolean active = VariabiliGlobali.getInstance().getDevicePolicyManager().isAdminActive(VariabiliGlobali.getInstance().getCompName());
 
         if (active) {
